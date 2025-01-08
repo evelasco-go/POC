81f7f5ee6418c7ee617d5a74d347b6ccbdaa2b46
evelasco-go <evelasco@gohealthuc.com>
2025-01-09 02:40:48 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
 provider "azurerm" {
+  client_id       = var.AZURE_CLIENT_ID
+  client_secret   = var.AZURE_CLIENT_SECRET
+  tenant_id       = var.AZURE_TENANT_ID
+  subscription_id = var.AZURE_SUBSCRIPTION_ID
+
   features {}
 }
 
