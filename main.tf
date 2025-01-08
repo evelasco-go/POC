2cfe0d5dea8cab287164f717afbd6d3ea361f208
evelasco-go <evelasco@gohealthuc.com>
2025-01-09 05:29:37 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "POCMyResourceGroup"
+    storage_account_name = "pocmystorageacct123"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
 
