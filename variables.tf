80e91aa202ef1de8433a045fda6a67cdec8c4b18
evelasco-go <evelasco@gohealthuc.com>
2025-01-09 03:10:00 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,28 +1,10 @@
-variable "AZURE_CLIENT_ID" {}
-variable "AZURE_CLIENT_SECRET" {}
-variable "AZURE_TENANT_ID" {}
-variable "AZURE_SUBSCRIPTION_ID" {}
-
-variable "resource_group_name" {
-  default = "POCMyResourceGroup"
-}
-
-variable "location" {
-  default = "East US"
-}
-
-variable "storage_account_name" {
-  default = "pocmystorageacct123"
-}
-
-variable "container_name" {
-  default = "tfstate"
-}
-
-variable "aks_name" {
-  default = "MyAKSCluster"
-}
-
-variable "node_count" {
-  default = 2
-}
+variable "azure_subscription_id" {}
+variable "azure_client_id" {}
+variable "azure_client_secret" {}
+variable "azure_tenant_id" {}
+variable "resource_group_name" {}
+variable "storage_account_name" {}
+variable "container_name" {}
+variable "aks_name" {}
+variable "node_count" {}
+variable "location" {}
