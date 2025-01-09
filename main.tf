55b0993371f16973a70bde5e1e728c7ae1cd016a
evelasco-go <evelasco@gohealthuc.com>
2025-01-09 22:09:40 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,26 +5,33 @@ terraform {
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0" # Ensure compatibility with your Terraform version
+    }
+  }
 }
 
 provider "azurerm" {
   features {}
 
-  subscription_id = var.azure_subscription_id
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
-  tenant_id       = var.azure_tenant_id
+  subscription_id = "your-subscription-id"  # Replace with your Azure Subscription ID
+  client_id       = "your-client-id"        # Replace with your Azure Client ID
+  client_secret   = "your-client-secret"    # Replace with your Azure Client Secret
+  tenant_id       = "your-tenant-id"        # Replace with your Azure Tenant ID
 }
 
 # Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = "example-resource-group"
+  location = "East US"
 }
 
 # Storage Account for Terraform State
 resource "azurerm_storage_account" "storage" {
-  name                     = var.storage_account_name
+  name                     = "examplestorageacct123"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
@@ -33,76 +40,25 @@ resource "azurerm_storage_account" "storage" {
 
 # Storage Container for State File
 resource "azurerm_storage_container" "container" {
-  name                  = var.container_name
+  name                  = "tfstate"
   storage_account_name  = azurerm_storage_account.storage.name
   container_access_type = "private"
 }
 
 # AKS Cluster
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.aks_name
+  name                = "example-aks-cluster"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "${var.aks_name}-dns"
+  dns_prefix          = "example-aks-dns"
 
   default_node_pool {
     name       = "default"
-    node_count = var.node_count
+    node_count = 2
     vm_size    = "Standard_DS2_v2"
   }
 
   identity {
     type = "SystemAssigned"
   }
-}
-
-# Variables
-variable "azure_subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-}
-
-variable "azure_client_id" {
-  description = "Azure Client ID"
-  type        = string
-}
-
-variable "azure_client_secret" {
-  description = "Azure Client Secret"
-  type        = string
-}
-
-variable "azure_tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "Name of the Resource Group"
-  type        = string
-}
-
-variable "storage_account_name" {
-  description = "Name of the Storage Account for Terraform State"
-  type        = string
-}
-
-variable "container_name" {
-  description = "Name of the Storage Container for Terraform State"
-  type        = string
-}
-
-variable "aks_name" {
-  description = "Name of the AKS Cluster"
-  type        = string
-}
-
-variable "node_count" {
-  description = "Number of nodes in the AKS cluster"
-  type        = number
-}
-
-variable "location" {
-  description = "Azure location for the resources"
-  type        = string
 }
