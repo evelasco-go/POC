aac2becf760fb7f8bef80639c3926757d93bf9d9
evelasco-go <evelasco@gohealthuc.com>
2025-01-14 22:51:27 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,43 @@
+# Provider and authentication setup
 provider "azurerm" {
   features {}
-
-  # Hardcoded Azure credentials
   client_id       = "9a7b7fdd-5a88-46e3-8d9b-b78042012e30"
   client_secret   = "s6h8Q~WNY_QKu92SobDd7FnfSIWJsYSYmKeF2dw0"
   tenant_id       = "fd3a4a13-0cd8-4c1c-ba4c-e4995f5ee282"
   subscription_id = "15e60859-88d7-4c84-943f-55488479910c"
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-akstest"
-  location = "East US"
+# Declare variables used in the configuration
+variable "aks_name" {
+  description = "Name of the AKS cluster"
+  type        = string
+  default     = "myakscluster"
 }
 
-resource "azurerm_storage_account" "storage" {
-  name                     = "aksstoragedemo"
-  resource_group_name       = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier               = "Standard"
-  account_replication_type = "LRS"
+variable "aks_location" {
+  description = "Location for the AKS cluster"
+  type        = string
+  default     = "East US"
 }
 
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "aks-demo"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "aksdemodns"
+variable "resource_group_name" {
+  description = "Resource group for the AKS cluster"
+  type        = string
+  default     = "myResourceGroup"
+}
+
+# Create the resource group
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.aks_location
+}
+
+# Create the AKS cluster
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = var.aks_name
+  location            = var.aks_location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "aks-cluster"
 
   default_node_pool {
     name       = "default"
@@ -36,12 +48,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
   identity {
     type = "SystemAssigned"
   }
-
-  tags = {
-    environment = "dev"
-  }
 }
 
+# Output the kubeconfig (marked as sensitive)
 output "kubeconfig" {
-  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+  value     = azurerm_kubernetes_cluster.example.kube_config.0.raw_kube_config
+  sensitive = true
 }
