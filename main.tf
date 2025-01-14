a8d0d4271c150594faeecd8ad6b88fa8721e4fdb
evelasco-go <evelasco@gohealthuc.com>
2025-01-14 23:40:12 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,117 +1,59 @@
-# Provider and authentication setup
-provider "azurerm" {
-  features {}
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
-  tenant_id       = var.azure_tenant_id
-  subscription_id = var.azure_subscription_id
+# Declare the Log Analytics Workspace
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = "goreg-test-analytics-workspace2"  # Name of the new workspace
+  location            = "East US"                          # Location for the workspace
+  resource_group_name = "POCMyResourceGroup"              # Resource group for the workspace
+  sku                 = "PerGB2018"                       # SKU for the workspace
 }
 
-# Declare variables used in the configuration
-variable "aks_name" {
-  description = "Name of the AKS cluster"
-  type        = string
-  default     = "myakscluster"
+# Declare a new Diagnostic Setting for AKS monitoring
+resource "azurerm_monitor_diagnostic_setting" "aks_monitoring" {
+  name               = "aks-diagnostic-setting"            # Name of the diagnostic setting
+  target_resource_id = azurerm_kubernetes_cluster.example.id  # AKS cluster ID
+
+  # Enable metric collection
+  metric {
+    category = "AllMetrics"  # You can specify specific categories like "ContainerInsights" or "KubeControllerManager"
+    enabled  = true
+  }
+
+  # Enable log collection
+  log {
+    category = "AuditLogs"    # Choose the appropriate log category for AKS
+    enabled  = true
+  }
+
+  log_analytics_workspace_id     = azurerm_log_analytics_workspace.example.id  # Log Analytics Workspace ID
+  log_analytics_destination_type = "Dedicated"  # This specifies where the logs will go (Dedicated workspace)
 }
 
-variable "aks_location" {
-  description = "Location for the AKS cluster"
-  type        = string
-  default     = "East US"
+# Declare the Storage Account (for storing Terraform state)
+resource "azurerm_storage_account" "example" {
+  name                     = "pocmystorageacct123"         # Storage account name
+  resource_group_name      = "POCMyResourceGroup"           # Resource group for the storage account
+  location                 = "East US"                      # Location for the storage account
+  account_tier              = "Standard"                    # Account tier for storage account
+  account_replication_type = "LRS"                          # Replication type for the storage account
 }
 
-variable "resource_group_name" {
-  description = "Resource group for the AKS cluster"
-  type        = string
-  default     = "myResourceGroup"
+# Declare the Storage Container for Terraform state
+resource "azurerm_storage_container" "example" {
+  name                  = "tfstate"                      # Container name
+  storage_account_name  = azurerm_storage_account.example.name  # Link the storage account
+  container_access_type = "private"                         # Container access type
 }
 
-variable "azure_client_id" {
-  description = "Azure Client ID"
-  type        = string
-}
-
-variable "azure_client_secret" {
-  description = "Azure Client Secret"
-  type        = string
-}
-
-variable "azure_tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-}
-
-variable "azure_subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-}
-
-variable "container_name" {
-  description = "Name of the storage container"
-  type        = string
-  default     = "mycontainer"
-}
-
-variable "storage_account_name" {
-  description = "Name of the storage account"
-  type        = string
-  default     = "mystorageaccount"
-}
-
-variable "location" {
-  description = "Location for the AKS and other resources"
-  type        = string
-  default     = "East US"
-}
-
-variable "node_count" {
-  description = "Number of nodes in the AKS cluster"
-  type        = number
-  default     = 2
-}
-
-# Create the resource group
-resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-# Create the AKS cluster
+# Declare AKS Cluster
 resource "azurerm_kubernetes_cluster" "example" {
-  name                = var.aks_name
-  location            = var.location
-  resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = "aks-cluster"
-
+  name                = "MyAKSCluster"                   # AKS Cluster name
+  location            = "East US"                         # Location for the AKS cluster
+  resource_group_name = "POCMyResourceGroup"             # Resource group for the AKS cluster
+  dns_prefix          = "exampleaks"                     # DNS prefix for the cluster
   default_node_pool {
     name       = "default"
-    node_count = var.node_count
+    node_count = 3
     vm_size    = "Standard_DS2_v2"
   }
 
-  identity {
-    type = "SystemAssigned"
-  }
-}
-
-# Output the kubeconfig (updated to reflect correct attribute)
-output "kubeconfig" {
-  value     = azurerm_kubernetes_cluster.example.kube_config
-  sensitive = true
-}
-
-# Create the storage account (for example)
-resource "azurerm_storage_account" "example" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = var.location
-  account_tier              = "Standard"
-  account_replication_type = "LRS"
-}
-
-# Create the storage container (for example)
-resource "azurerm_storage_container" "example" {
-  name                  = var.container_name
-  storage_account_name  = azurerm_storage_account.example.name
-  container_access_type = "private"
+  # Add other AKS configuration as required
 }
