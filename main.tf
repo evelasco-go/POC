58286ce27fb720e19fba86d9f1ae5401f5126e17
evelasco-go <evelasco@gohealthuc.com>
2025-01-14 22:36:20 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,59 +1,29 @@
-terraform {
-  required_version = ">= 1.5.7, < 2.0.0"
-
-  backend "azurerm" {
-    resource_group_name  = "POCMyResourceGroup"
-    storage_account_name = "pocmystorageacct123"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
-# Azure Provider Configuration
+# Provider Configuration
 provider "azurerm" {
   features {}
-
-  subscription_id = "15e60859-88d7-4c84-943f-55488479910c"
-  client_id       = "9a7b7fdd-5a88-46e3-8d9b-b78042012e30"
-  client_secret   = "s6h8Q~WNY_QKu92SobDd7FnfSIWJsYSYmKeF2dw0"
-  tenant_id       = "fd3a4a13-0cd8-4c1c-ba4c-e4995f5ee282"
 }
 
 # Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "POCMyResourceGroup"
-  location = "eastus"
+  name     = "rg-akstest"
+  location = "East US"
 }
 
 # Storage Account
 resource "azurerm_storage_account" "storage" {
-  name                     = "pocmystorageacct123"
-  resource_group_name      = azurerm_resource_group.rg.name
+  name                     = "aksstoragedemo"
+  resource_group_name       = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
+  account_tier               = "Standard"
   account_replication_type = "LRS"
 }
 
-# Storage Container
-resource "azurerm_storage_container" "container" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.storage.name
-  container_access_type = "private"
-}
-
-# Azure Kubernetes Cluster (AKS)
+# Azure Kubernetes Service (AKS)
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "example-aks-cluster"
+  name                = "aks-demo"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "example-aks-cluster-dns"
+  dns_prefix          = "aksdemodns"
 
   default_node_pool {
     name       = "default"
@@ -64,4 +34,13 @@ resource "azurerm_kubernetes_cluster" "aks" {
   identity {
     type = "SystemAssigned"
   }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+# Output the AKS cluster kubeconfig
+output "kubeconfig" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
 }
