b11e62e5e4534024861d759a187857ddf8b35c25
evelasco-go <evelasco@gohealthuc.com>
2025-01-16 22:02:08 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -112,7 +112,7 @@ resource "azurerm_storage_account" "example" {
 # Create the storage container (for example)
 resource "azurerm_storage_container" "example" {
   name                  = var.container_name
-  storage_account_name  = azurerm_storage_account.example.name
+  storage_account_id    = azurerm_storage_account.example.id
   container_access_type = "private"
 }
 
