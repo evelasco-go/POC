6eb6d0e035aef7934cf90b1fbf3189868c743b78
evelasco-go <evelasco@gohealthuc.com>
2025-01-17 23:27:19 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -106,7 +106,7 @@ resource "azurerm_storage_container" "example" {
 
 # Declare new Log Analytics workspace
 resource "azurerm_log_analytics_workspace" "example" {
-  name                = "goreg-test-analytics-workspace22"
+  name                = "poc-analytics"
   location            = var.location
   resource_group_name = azurerm_resource_group.example.name
   sku                 = "PerGB2018"
@@ -114,7 +114,7 @@ resource "azurerm_log_analytics_workspace" "example" {
 
 # Create the metrics diagnostic setting
 resource "azurerm_monitor_diagnostic_setting" "aks_metrics" {
-  name               = "aks-metrics-diagnostic-setting2"
+  name               = "poc-aks-metrics-diagnostic"
   target_resource_id = azurerm_kubernetes_cluster.example.id
 
   metric {
