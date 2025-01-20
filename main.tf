f6236122d5994b5866d7be7869a5b67159a741d0
evelasco-go <evelasco@gohealthuc.com>
2025-01-20 22:27:33 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -78,11 +78,6 @@ variable "subscription_id" {
   type        = string
 }
 
-variable "resource_group_name" {
-  description = "The name of the resource group."
-  type        = string
-}
-
 variable "aks_cluster_name" {
   description = "The name of the AKS cluster."
   type        = string
