5d3fa0f644f58bc0192b0ac15b2d6f364664e557
evelasco-go <evelasco@gohealthuc.com>
2025-01-20 23:13:05 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -120,12 +120,17 @@ output "kubeconfig" {
 
 # Storage Account Resource
 resource "azurerm_storage_account" "example" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.example.name
+  name                     = "pocstorage${random_id.unique_suffix.hex}"  # Generate a unique name
+  resource_group_name       = var.resource_group_name
   location                 = var.location
   account_tier              = "Standard"
   account_replication_type = "LRS"
 }
+
+resource "random_id" "unique_suffix" {
+  byte_length = 8
+}
+
 
 # Storage Container Resource
 resource "azurerm_storage_container" "example" {
