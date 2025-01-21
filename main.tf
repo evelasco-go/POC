d32d5ff93b0918b8398a58bea5f34d15c5b60489
evelasco-go <evelasco@gohealthuc.com>
2025-01-21 22:12:46 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,13 +9,6 @@ terraform {
       version = "~> 3.0"
     }
   }
-
-  backend "azurerm" {
-    resource_group_name  = var.resource_group_name
-    storage_account_name = var.storage_account_name
-    container_name       = var.container_name
-    key                  = "terraform.tfstate"
-  }
 }
 
 provider "azurerm" {
