19c5c237917c726d16ed54b4d8a407fda20e3518
evelasco-go <evelasco@gohealthuc.com>
2025-01-21 22:26:28 +0800

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "pcPOCresourcepcpcpcpoc"
-    storage_account_name = "pcpocstoragepcpcpcpoc"
-    container_name       = "pctfstate"
-    key                  = "terraform.tfstate"
-  }
-}
