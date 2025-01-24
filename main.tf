94a3424c8f8b7f1933564e33ecd94d3c3b2f8f74
evelasco-go <evelasco@gohealthuc.com>
2025-01-25 02:17:18 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~> 3.0"  # Specify a version here
-    }
-    random = {
-      source = "hashicorp/random"
-      version = "~> 3.0"  # Specify a version here
-    }
-  }
-}
-
-# Provider configuration for Azure
 provider "azurerm" {
   features {}
   client_id       = var.azure_client_id
@@ -20,25 +6,18 @@ provider "azurerm" {
   subscription_id = var.azure_subscription_id
 }
 
-# Provider configuration for random ID generation
 provider "random" {}
 
-# Define variables
 variable "azure_subscription_id" {}
 variable "azure_client_id" {}
 variable "azure_client_secret" {}
 variable "azure_tenant_id" {}
 variable "resource_group_name" {}
-variable "storage_account_name" {}
-variable "container_name" {}
 variable "aks_name" {}
-variable "node_count" {}
 variable "location" {}
 variable "log_analytics_workspace_name" {}
-variable "log_analytics_sku" {}
-variable "diagnostic_setting_name" {}
+variable "grafana_instance_name" {}
 
-# Resource group
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
@@ -53,53 +32,53 @@ resource "azurerm_kubernetes_cluster" "example" {
 
   default_node_pool {
     name       = "default"
-    node_count = var.node_count
+    node_count = 2
     vm_size    = "Standard_DS2_v2"
   }
 
   identity {
     type = "SystemAssigned"
   }
-}
 
-# Output kubeconfig (sensitive)
-output "kubeconfig" {
-  value     = azurerm_kubernetes_cluster.example.kube_config
-  sensitive = true
-}
+  addon_profiles {
+    monitoring {
+      enabled = true
+      config {
+        log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
+      }
+    }
 
+    managed_prometheus {
+      enabled = true
+    }
 
-# Azure Storage Account
-resource "azurerm_storage_account" "example" {
-  name                     = var.storage_account_name
-  resource_group_name       = var.resource_group_name
-  location                 = var.location
-  account_tier              = "Standard"
-  account_replication_type = "LRS"
-}
-
-# Storage Container Resource
-resource "azurerm_storage_container" "example" {
-  name                  = var.container_name
-  storage_account_name  = var.storage_account_name
-  container_access_type = "private"
-  lifecycle {
-    prevent_destroy = true
+    grafana {
+      enabled = true
+    }
   }
 }
 
+# Managed Grafana instance
+resource "azurerm_monitor_grafana" "example" {
+  name                = var.grafana_instance_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.example.name
+  identity {
+    type = "SystemAssigned"
+  }
+}
 
 # Log Analytics Workspace Resource
 resource "azurerm_log_analytics_workspace" "example" {
   name                = var.log_analytics_workspace_name
   location            = var.location
   resource_group_name = azurerm_resource_group.example.name
-  sku                 = var.log_analytics_sku
+  sku                 = "PerGB2018"
 }
 
 # Monitor Diagnostic Setting Resource for AKS
 resource "azurerm_monitor_diagnostic_setting" "example" {
-  name               = var.diagnostic_setting_name
+  name               = "aks-diagnostics"
   target_resource_id = "/subscriptions/${var.azure_subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.ContainerService/managedClusters/${var.aks_name}"
 
   metric {
