a3286758bb58f44762b57cc57a5fc1fea5cfba74
evelasco-go <evelasco@gohealthuc.com>
2025-01-28 00:39:31 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,87 +1,80 @@
 provider "azurerm" {
   features {}
-  subscription_id = "15e60859-88d7-4c84-943f-55488479910c"
-  client_id       = "9a7b7fdd-5a88-46e3-8d9b-b78042012e30"
-  client_secret   = "s6h8Q~WNY_QKu92SobDd7FnfSIWJsYSYmKeF2dw0"
-  tenant_id       = "fd3a4a13-0cd8-4c1c-ba4c-e4995f5ee282"
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+  subscription_id = var.azure_subscription_id
 }
 
-provider "kubernetes" {
-  config_path = "~/.kube/config"  # Adjust if necessary
-}
-
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"  # Adjust if necessary
-  }
-}
-
+# Resource group
 resource "azurerm_resource_group" "example" {
-  name     = "Goreg4"
-  location = "East US"
+  name     = var.resource_group_name
+  location = var.location
 }
 
+# Azure Storage Account
 resource "azurerm_storage_account" "example" {
-  name                     = "goreg4"
+  name                     = var.storage_account_name
   resource_group_name       = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  location                 = var.location
   account_tier              = "Standard"
   account_replication_type = "LRS"
 }
 
+# Storage Container Resource
 resource "azurerm_storage_container" "example" {
-  name                   = "goreg4container"
-  storage_account_id     = azurerm_storage_account.example.id  # Use ID instead of name
-  container_access_type  = "private"
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
 }
 
+# Azure Kubernetes Service (AKS) Cluster
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  dns_prefix = "${var.aks_name}-dns"
+}
+
+# Log Analytics Workspace
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = var.log_analytics_workspace_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku                 = var.log_analytics_sku
+}
+
+# Diagnostic Setting for AKS
 resource "azurerm_monitor_diagnostic_setting" "aks_metrics" {
-  name                       = "goreg4-diagnostics"
-  target_resource_id         = azurerm_kubernetes_cluster.example.id
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
+  name               = var.diagnostic_setting_name
+  target_resource_id = azurerm_kubernetes_cluster.example.id
+
   metric {
     category = "AllMetrics"
     enabled  = true
   }
-  #log_analytics {
-  #  category = "KubeInventory"
-  #  enabled  = true
-  #}
+
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
 }
 
-resource "azurerm_kubernetes_cluster" "example" {
-  name                = "goreg4-aks"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = "goreg4"
-  linux_profile {
-    admin_username = "azureuser"
-    ssh_key {
-      key_data = "ssh-rsa ... your SSH key ...="
-    }
-  }
-  default_node_pool {
-    name       = "default"
-    node_count = 2
-    vm_size    = "Standard_DS2_v2"
-  }
-}
-
+# Helm Chart Installation (Prometheus & Grafana)
 resource "helm_release" "prometheus" {
   name       = "prometheus"
   namespace  = "monitoring"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
-  version    = "40.0.0"
-  values = [
-    {
-      "prometheus" = {
-        "service" = {
-          "type" = "LoadBalancer"
-        }
-      }
-    }
-  ]
+  version    = "15.1.0"
   wait       = true
 }
 
@@ -91,10 +84,9 @@ resource "helm_release" "grafana" {
   repository = "https://grafana.github.io/helm-charts"
   chart      = "grafana"
   version    = "6.19.3"
-  values = [
-    {
-      "adminPassword" = "yourpassword"
-    }
+  values     = [
+    "adminPassword=yourpassword",
+    "service.type=LoadBalancer"
   ]
   wait       = true
 }
