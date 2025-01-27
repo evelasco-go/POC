84d52d97683ae32f1bac3836840d91285514106e
evelasco-go <evelasco@gohealthuc.com>
2025-01-28 06:14:54 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,14 +22,14 @@ resource "azurerm_storage_account" "example" {
   account_replication_type = "LRS"
 }
 
-# Storage Container Resource
+# Storage Container
 resource "azurerm_storage_container" "example" {
   name                  = var.container_name
   storage_account_name  = azurerm_storage_account.example.name
   container_access_type = "private"
 }
 
-# Virtual Network for AKS
+# Virtual Network
 resource "azurerm_virtual_network" "example" {
   name                = "${var.resource_group_name}-vnet"
   location            = azurerm_resource_group.example.location
@@ -45,17 +45,18 @@ resource "azurerm_subnet" "example" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# Azure Kubernetes Service (AKS) Cluster
+# AKS Cluster
 resource "azurerm_kubernetes_cluster" "example" {
   name                = var.aks_name
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
   default_node_pool {
-    name       = "default"
-    node_count = var.node_count
-    vm_size    = "Standard_DS2_v2"
-    vnet_subnet_id = azurerm_subnet.example.id
+    name                       = "default"
+    temporary_name_for_rotation = "default-rotated" # Temporary name for rotation
+    node_count                 = var.node_count
+    vm_size                    = "Standard_DS2_v2"
+    vnet_subnet_id             = azurerm_subnet.example.id
   }
 
   identity {
@@ -65,7 +66,7 @@ resource "azurerm_kubernetes_cluster" "example" {
   dns_prefix = "${var.aks_name}-dns"
 }
 
-# Role Assignment for AKS System-Assigned Identity
+# Role Assignment for AKS
 resource "azurerm_role_assignment" "example" {
   principal_id         = azurerm_kubernetes_cluster.example.kubelet_identity[0].object_id
   role_definition_name = "Contributor"
@@ -80,7 +81,7 @@ resource "azurerm_log_analytics_workspace" "example" {
   sku                 = var.log_analytics_sku
 }
 
-# Diagnostic Setting for AKS
+# Diagnostic Settings for AKS
 resource "azurerm_monitor_diagnostic_setting" "aks_metrics" {
   name               = var.diagnostic_setting_name
   target_resource_id = azurerm_kubernetes_cluster.example.id
@@ -93,29 +94,20 @@ resource "azurerm_monitor_diagnostic_setting" "aks_metrics" {
   log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
 }
 
-# Fetch AKS credentials
+# Fetch AKS Credentials
 resource "null_resource" "get_aks_credentials" {
   provisioner "local-exec" {
     command = <<EOT
-      az aks get-credentials --resource-group ${var.resource_group_name} --name ${var.aks_name}
+      az aks get-credentials --resource-group ${var.resource_group_name} --name ${var.aks_name} --admin --overwrite-existing
     EOT
   }
 
   triggers = {
-    always_run = "${timestamp()}"
+    always_run = timestamp()
   }
 }
 
-default_node_pool {
-  name                       = "default"
-  temporary_name_for_rotation = "default-rotated" # Temporary name for rotation
-  node_count                 = var.node_count
-  vm_size                    = "Standard_DS2_v2"
-  vnet_subnet_id             = azurerm_subnet.example.id
-}
-
-
-# Helm Chart Installation (Prometheus & Grafana)
+# Helm Release for Prometheus
 resource "helm_release" "prometheus" {
   depends_on = [null_resource.get_aks_credentials]
 
@@ -127,6 +119,7 @@ resource "helm_release" "prometheus" {
   wait       = true
 }
 
+# Helm Release for Grafana
 resource "helm_release" "grafana" {
   depends_on = [null_resource.get_aks_credentials]
 
@@ -145,18 +138,11 @@ EOF
   wait       = true
 }
 
-# Output values
+# Output Values
 output "aks_cluster_name" {
   value = azurerm_kubernetes_cluster.example.name
 }
 
-output "aks_cluster_kube_config" {
-  value = <<EOT
-Run the following command to use kubectl with this cluster:
-export KUBECONFIG=$(pwd)/kubeconfig
-EOT
-}
-
 output "grafana_url" {
-  value = helm_release.grafana.name
+  value = "Grafana installed at LoadBalancer. Check the external IP in your Kubernetes cluster."
 }
