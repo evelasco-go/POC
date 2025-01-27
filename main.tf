a7439df99fdbc9cf8b53860842603b9ffdddc3fa
evelasco-go <evelasco@gohealthuc.com>
2025-01-27 21:43:41 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,6 +23,14 @@ resource "azurerm_resource_group" "example" {
   location = var.location
 }
 
+# Log Analytics Workspace Resource
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = var.log_analytics_workspace_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku                 = "PerGB2018"
+}
+
 # Azure Kubernetes Service Cluster
 resource "azurerm_kubernetes_cluster" "example" {
   name                = var.aks_name
@@ -68,18 +76,35 @@ resource "azurerm_monitor_grafana" "example" {
   }
 }
 
-# Log Analytics Workspace Resource
-resource "azurerm_log_analytics_workspace" "example" {
-  name                = var.log_analytics_workspace_name
-  location            = var.location
-  resource_group_name = azurerm_resource_group.example.name
-  sku                 = "PerGB2018"
-}
-
 # Monitor Diagnostic Setting Resource for AKS
 resource "azurerm_monitor_diagnostic_setting" "example" {
   name               = "aks-diagnostics"
-  target_resource_id = "/subscriptions/${var.azure_subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.ContainerService/managedClusters/${var.aks_name}"
+  target_resource_id = azurerm_kubernetes_cluster.example.id
+
+  log {
+    category = "kube-apiserver"
+    enabled  = true
+  }
+
+  log {
+    category = "kube-controller-manager"
+    enabled  = true
+  }
+
+  log {
+    category = "kube-scheduler"
+    enabled  = true
+  }
+
+  log {
+    category = "cluster-autoscaler"
+    enabled  = true
+  }
+
+  log {
+    category = "guard"
+    enabled  = true
+  }
 
   metric {
     category = "AllMetrics"
