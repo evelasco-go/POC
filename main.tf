b8c04eeeb66108a428228d601212d453df63056b
evelasco-go <evelasco@gohealthuc.com>
2025-01-27 23:56:26 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,75 @@
 provider "azurerm" {
   features {}
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
-  tenant_id       = var.azure_tenant_id
-  subscription_id = var.azure_subscription_id
+  client_id       = "9a7b7fdd-5a88-46e3-8d9b-b78042012e30"
+  client_secret   = "s6h8Q~WNY_QKu92SobDd7FnfSIWJsYSYmKeF2dw0"
+  tenant_id       = "fd3a4a13-0cd8-4c1c-ba4c-e4995f5ee282"
+  subscription_id = "15e60859-88d7-4c84-943f-55488479910c"
 }
 
 # Define variables
-variable "azure_subscription_id" {}
-variable "azure_client_id" {}
-variable "azure_client_secret" {}
-variable "azure_tenant_id" {}
-variable "resource_group_name" {}
-variable "storage_account_name" {}
-variable "container_name" {}
-variable "aks_name" {}
-variable "log_analytics_workspace_name" {}
-variable "diagnostic_setting_name" {}
+variable "azure_subscription_id" {
+  default = "15e60859-88d7-4c84-943f-55488479910c"
+}
+
+variable "azure_client_id" {
+  default = "9a7b7fdd-5a88-46e3-8d9b-b78042012e30"
+}
+
+variable "azure_client_secret" {
+  default = "s6h8Q~WNY_QKu92SobDd7FnfSIWJsYSYmKeF2dw0"
+}
+
+variable "azure_tenant_id" {
+  default = "fd3a4a13-0cd8-4c1c-ba4c-e4995f5ee282"
+}
+
+variable "resource_group_name" {
+  default = "Goreg4"
+}
+
+variable "storage_account_name" {
+  default = "goreg4"
+}
+
+variable "container_name" {
+  default = "goreg4container"
+}
+
+variable "aks_name" {
+  default = "goreg4-aks"
+}
+
+variable "log_analytics_workspace_name" {
+  default = "goreg4-analytics"
+}
+
+variable "diagnostic_setting_name" {
+  default = "goreg4-diagnostics"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "log_analytics_sku" {
+  default = "PerGB2018"
+}
+
+variable "node_count" {
+  default = 2
+}
 
 # Resource group
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
-  location = "East US"
+  location = var.location
 }
 
 # Azure Storage Account
 resource "azurerm_storage_account" "example" {
   name                     = var.storage_account_name
   resource_group_name       = azurerm_resource_group.example.name
-  location                 = "East US"
+  location                 = var.location
   account_tier              = "Standard"
   account_replication_type = "LRS"
 }
@@ -48,7 +89,7 @@ resource "azurerm_kubernetes_cluster" "example" {
 
   default_node_pool {
     name       = "default"
-    node_count = 2
+    node_count = var.node_count
     vm_size    = "Standard_DS2_v2"
   }
 
@@ -69,7 +110,7 @@ resource "azurerm_log_analytics_workspace" "example" {
   name                = var.log_analytics_workspace_name
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  sku                 = "PerGB2018"
+  sku                 = var.log_analytics_sku
 }
 
 # Diagnostic Setting for AKS
@@ -132,4 +173,3 @@ scrape_configs:
 YAML
   }
 }
-
