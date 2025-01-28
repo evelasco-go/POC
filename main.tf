04ddb8846699b1b47fa94852bc6d0fa2e124f85b
evelasco-go <evelasco@gohealthuc.com>
2025-01-29 02:36:28 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "azurerm_monitor_diagnostic_setting" "example" {
   target_resource_id         = azurerm_kubernetes_cluster.example.id
   log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
 
-  metrics {
+  metric {
     category = "AllMetrics"
     enabled  = true
     retention_policy {
